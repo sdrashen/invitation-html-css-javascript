5669621c0ba0db623ddebeacbd1e209f387a0096
Sabrina Drashen <sdrashen@gmail.com>
2022-02-19 22:25:43 -0300

    Created function Javascript

---
+++ main.js
@@ -0,0 +1,5 @@
+function saveHomeData() {
+    let dataFrom = document.getElementById('from').value
+    let dataTo = document.getElementById('to').value
+    let dataMessage = document.getElementById('message').value
+}
